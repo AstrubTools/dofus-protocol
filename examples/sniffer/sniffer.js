4ed24eeda1fb6d4a58e8f29ad59ff096b546a1a7
louis <louis.beaumont@gmail.com>
2019-11-03 19:44:15 +0300

    Splitter for toServer, sniffer clean

--- examples/sniffer/sniffer.js
+++ examples/sniffer/sniffer.js
@@ -1,13 +1,15 @@
-const { defaultVersion, protocol, dofus, split } = require('../..')
+const { defaultVersion, protocol, dofus, Splitter } = require('../..')
 
-if (process.argv.length !== 3) {
-  console.log('Usage : node sniffer.js <networkInterface>')
+if (process.argv.length !== 4) {
+  console.log('Usage : node sniffer.js <networkInterface> <dev>')
   process.exit(1)
 }
 // If the version correspond to a supported version else use default
 const version = defaultVersion
 
 const networkInterface = process.argv[2]
+
+const dev = process.argv[3]
 
 const pcap = require('pcap')
 
@@ -17,47 +19,51 @@ const pcapSession = pcap.createSession(networkInterface, 'ip proto \\tcp')
 const ProtoDef = require('protodef').ProtoDef
 
 const toServer = new ProtoDef(false)
-toServer.addProtocol(protocol[version].data, ['toServer'])
 toServer.addTypes(dofus)
+toServer.addProtocol(protocol[version].data, ['toServer'])
+const splitterToServer = new Splitter(/\r?\n?\0/)
 
 const toClient = new ProtoDef(false)
-toClient.addProtocol(protocol[version].data, ['toClient'])
 toClient.addTypes(dofus)
+toClient.addProtocol(protocol[version].data, ['toClient'])
+const splitterToClient = new Splitter(/\r?\0/)
 
-split.on('data', data => {
-  console.log('raw', 'toClient : ', data.toString('ascii'))
+function display (data, isToServer) {
+  const s = isToServer ? 'toServer : ' : 'toClient : '
+  console.log(`~`.repeat(10))
   try {
-    const parsed = toClient.parsePacketBuffer('packet', data).data
-    console.info('toClient : ', JSON.stringify(parsed))
+    const parsed = isToServer ? toServer.parsePacketBuffer('packet', data).data : toClient.parsePacketBuffer('packet', data).data
+    console.info(s, JSON.stringify(parsed))
   } catch (error) {
     console.log(error.message)
   }
+  console.log(`raw ${s} ${data.toString('ascii')}`)
+  console.log(`~`.repeat(10), '\n')
+}
+
+splitterToServer.on('data', data => {
+  display(data, true)
+})
+splitterToClient.on('data', data => {
+  display(data, false)
 })
 
-// const IP = '34.251.172.139' // Official dofus retro
-const IP = '190.115.26.126' // Amakna server
+const ipOfficial = '34.251.172.139' // Official dofus retro
+const ipPrivate = '190.115.26.126' // Amakna server
+const ip = dev ? ipPrivate : ipOfficial
 
-const PORT = 443
 pcapSession.on('packet', function (rawPacket) {
   const packet = pcap.decode.packet(rawPacket)
-  if (packet.payload.payload.saddr.addr.join('.') === IP) { // To client
-    let data = packet.payload.payload.payload.data
-    if (data === null) return
+  let data = packet.payload.payload.payload.data
+  if (!data) return
+  if (packet.payload.payload.saddr.addr.join('.') === ip) { // To client
     if ((data[0] === 0x3c && data[1] === 0x3f) || data[0] === 0xc3 || data[0] === 0xd1) { // 3c && 3f is trash ad begin login, c3 trash media priv server?
-      console.log('Ignore trash')
       return
     }
-    split.write(data)
-  } else if (packet.payload.payload.daddr.addr.join('.') === IP) { // To server
-    let data = packet.payload.payload.payload.data
-    try {
-      const parsed = toServer.parsePacketBuffer('packet', data).data
-      console.info('toServer : ', JSON.stringify(parsed))
-      console.log('raw toServer : ', data.toString('ascii'))
-    } catch (error) {
-      if (data) console.log('raw toServer', data.toString('ascii'))
-      console.log(error.message)
-    }
+    splitterToClient.write(data)
+    tcpTracker.track_packet(packet)
+  } else if (packet.payload.payload.daddr.addr.join('.') === ip) { // To server
+    splitterToServer.write(data)
     tcpTracker.track_packet(packet)
   }
 })
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { defaultVersion, supportedVersions } = require('./version')
 const createClient = require('./lib/client/createClient')
 const dofus = require('./lib/utils/datatypes/dofus')
-const split = require('./lib/transforms/framing')
+const Splitter = require('./lib/transforms/framing')
 
 const protocol = supportedVersions.reduce((acc, version) => {
   acc[version] = {
@@ -16,5 +16,5 @@ module.exports = {
   defaultVersion,
   createClient,
   dofus,
-  split
+  Splitter
 }
